bc2a6fa09e29060641b0e37eba4a63b7650bd274
Infinity4B <mail707293768@qq.com>
2026-07-21 16:06:06 +0800

    update: configure LIFT2 training for XYZW action stats
    Reject incompatible normalization metadata, avoid resuming legacy quaternion checkpoints, and isolate the new XYZW training outputs while applying the updated destination loss weight.

--- lingbot-va/wan_va/configs/va_lift2_merged_train_cfg.py
+++ lingbot-va/wan_va/configs/va_lift2_merged_train_cfg.py
@@ -34,6 +34,32 @@ with open(
 ) as action_norm_stats_file:
     action_norm_statistics = json.load(action_norm_stats_file)
 
+action_representation = action_norm_statistics.get('representation', {})
+expected_action_representation = {
+    'pose_mode': 'segment-relative',
+    'quaternion_order': 'xyzw',
+    'euler_axes': 'xyz',
+}
+for representation_key, expected_value in expected_action_representation.items():
+    actual_value = action_representation.get(representation_key)
+    if actual_value != expected_value:
+        raise ValueError(
+            'Action normalization statistics use an incompatible '
+            f'{representation_key}: expected {expected_value!r}, '
+            f'got {actual_value!r} in '
+            f'{va_lift2_merged_train_cfg.action_norm_stats_path}'
+        )
+
+if (
+    action_representation.get('used_action_channel_ids')
+    != va_lift2_merged_train_cfg.used_action_channel_ids
+):
+    raise ValueError(
+        'Action normalization statistics use incompatible action channels: '
+        f'expected {va_lift2_merged_train_cfg.used_action_channel_ids}, '
+        f'got {action_representation.get("used_action_channel_ids")}'
+    )
+
 va_lift2_merged_train_cfg.norm_stat = action_norm_statistics['norm_stat']
 if (
     len(va_lift2_merged_train_cfg.norm_stat['q01'])
@@ -70,13 +96,9 @@ va_lift2_merged_train_cfg.save_interval = 200
 va_lift2_merged_train_cfg.gc_interval = 50
 va_lift2_merged_train_cfg.cfg_prob = 0.1
 
-va_lift2_merged_train_cfg.resume_from = os.path.join(
-    '/soft/wangxi/4DWAM/lingbot-va/checkpoints',
-    'lift2_merged_lingbot_va',
-    'train_8_8_5000',
-    'checkpoints',
-    'checkpoint_step_2000',
-)
+# The existing LIFT2 checkpoints were trained with the legacy quaternion
+# representation. Start this XYZW run from the base LingBot-VA transformer.
+va_lift2_merged_train_cfg.resume_from = None
 
 va_lift2_merged_train_cfg.learning_rate = 1e-5
 va_lift2_merged_train_cfg.beta1 = 0.9
@@ -88,7 +110,7 @@ va_lift2_merged_train_cfg.gradient_accumulation_steps = 8
 va_lift2_merged_train_cfg.num_steps = 5000
 va_lift2_merged_train_cfg.align_layer = 20
 
-va_lift2_merged_train_cfg.keyword = 'lift2_merged_lingbot_va'
+va_lift2_merged_train_cfg.keyword = 'lift2_merged_lingbot_va_xyzw_v2'
 va_lift2_merged_train_cfg.save_root = os.path.join(
     '/soft/wangxi/4DWAM/lingbot-va/checkpoints',
     va_lift2_merged_train_cfg.keyword,
@@ -104,7 +126,7 @@ va_lift2_merged_train_cfg.save_root = os.path.join(
 va_lift2_merged_train_cfg.enable_trace = False
 va_lift2_merged_train_cfg.trace_coef = 0.0
 va_lift2_merged_train_cfg.loss_weights = {
-    'dest_loss': 0.01,
+    'dest_loss': 0.03,
     'motion_loss': 0.01,
     'trace_loss': 0.0,
 }
